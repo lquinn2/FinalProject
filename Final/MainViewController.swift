ffbcce5cfd925c5e37bd3fd4b384822a021bdbc4
Student <student@cvs-r212d011.nic.bc.ca>
2019-03-25 16:08:45 -0700

    Have collection view made but has errors

---
+++ Final/MainViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Final
+//
+//  Created by Student on 2019-03-05.
+//  Copyright © 2019 Liam Quinn. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        var cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as UICollectionViewCell
+        return cell
+    }
+
+
+}
+
